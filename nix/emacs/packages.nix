2076bd25262f250cb0f1181e15f40e182b00ac51
Juanjo Presa <juanjop@gmail.com>
2023-06-03 23:40:18 +0200

    emacs: clean packages

--- nix/emacs/packages.nix
+++ nix/emacs/packages.nix
@@ -30,98 +30,55 @@
   in
     lib.flatten (map recurse (fromElisp configText));
 
-  agitate = emacsPackages.elpaBuild {
-    pname = "agitate";
-    ename = "agitate";
-    version = "0.0.20221023.11841";
-    src = fetchurl {
-      url = "https://elpa.gnu.org/devel/agitate-0.0.20221023.11841.tar";
-      sha256 = "sha256-fCGnBUVqZtMI4RhYShsF0BUEzouJaJ61doA+nqGXGIc=";
-    };
-    packageRequires = [emacs];
-    meta = {
-      homepage = "https://elpa.gnu.org/packages/ace-window.html";
-      license = lib.licenses.free;
-    };
-  };
-
   extraPackages = epkgs:
     with epkgs;
       [
-        # prot
-        agitate # Not in ELPA yet
-        altcaps
-        beframe
-        bongo
+        ## prot
+        # agitate
+        # altcaps
+        # beframe
+        # cursory
+        # denote
+        # ef-themes
+        # fontaine
+        # lin
+        # logos
+        # mct
+        # modus-themes
+        # notmuch-indicator
+        # pulsar
+        # standard-themes
+        # sxhkdrc-mode
+        # tmr
+      ] ++ [
+        ## third-party
         cape
-        citar
-        citar-denote
-        citar-embark
         consult
-        consult-dir
         corfu
-        cursory
-        denote
-        diff-hl
         dired-subtree
-        ebdb
-        ef-themes
         elfeed
-        elfeed-tube
-        elfeed-tube-mpv
-        elpher
         embark
         embark-consult
-        evil
-        exec-path-from-shell
-        flymake-diagnostic-at-point
-        flymake-proselint
-        flymake-shellcheck
-        fontaine
         goto-last-change
 	      inputs.nixpkgs.emacsPackages.jinx
-        js-comint
         keycast
-        lin
-        logos
         magit
         marginalia
         markdown-mode
-        mct
-        minions
-        modus-themes
-        mpv
-        nix-mode
-        notmuch-indicator
-        notmuch-addr
         olivetti
         ol-notmuch
         orderless
-        org-modern
-        osm
         package-lint-flymake
         pass
         password-store
-        pcmpl-args
-        plz
-        pulsar
         rainbow-mode
-        recursion-indicator
-        sxhkdrc-mode
         substitute
         tempel
-        terraform-mode
-        tmr
-        transpose-frame
         trashed
         vertico
-        vterm
-        which-key
         wgrep
-        yaml-mode
-      ]
-      ++ [
-        # user
+      ] ++ [
+        ## user
         nix-mode
         terraform-mode
         vterm

72a2aaeeb212659ed905b776e7524c67fb56b22a
Juanjo Presa <juanjop@gmail.com>
2024-09-18 00:06:07 +0200

    use kodi-gbm

--- nix/kodi/packages.nix
+++ nix/kodi/packages.nix
@@ -14,24 +14,14 @@ in {
     myKodiPackages.movistarplus
   ]);
 
-  kodi-rpi = let
-    kodi = inputs.nixpkgs.kodi-wayland.override {
-      # vdpauSupport = false;
-      # x11Support = false;
-      # waylandSupport = true;
-      # sambaSupport = false;
-      # rtmpSupport = false;
-      # joystickSupport = false;
-    };
-  in
-    kodi.withPackages (exts: [
-      exts.pvr-iptvsimple
-      (exts.six.overrideAttrs { pythonPath = "lib"; })
-      (exts.toKodiAddon myKodiPackages.movistarplus)
-      (exts.toKodiAddon myKodiPackages.tml2ssa)
-      (exts.toKodiAddon myKodiPackages.pytz)
-      exts.dateutil
-      exts.requests
-      exts.inputstreamhelper
-    ]);
+  kodi-rpi = inputs.nixpkgs.kodi-gbm.withPackages (exts: [
+    exts.pvr-iptvsimple
+    (exts.six.overrideAttrs { pythonPath = "lib"; })
+    (exts.toKodiAddon myKodiPackages.movistarplus)
+    (exts.toKodiAddon myKodiPackages.tml2ssa)
+    (exts.toKodiAddon myKodiPackages.pytz)
+    exts.dateutil
+    exts.requests
+    exts.inputstreamhelper
+  ]);
 }

de70651b86f0621a2622dbc6ff1a6e03c0bbff3c
Juanjo Presa <juanjop@gmail.com>
2024-11-30 18:05:58 +0100

    update movistar plugin

--- nix/kodi/kodi-packages/movistarplus/default.nix
+++ nix/kodi/kodi-packages/movistarplus/default.nix
@@ -11,13 +11,13 @@
 }: buildKodiAddon rec {
   pname = "movistarplus";
   namespace = "plugin.video.movistarplus";
-  version = "0.9.8";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "Paco8";
     repo = namespace;
     rev = "v${version}";
-    hash = "sha256-NDqB8M+dcF6/2FGPEkeHMdfr/Z+DAxHcIBPDuGo3b0g=";
+    hash = "sha256-NDqB9M+dcF6/2FGPEkeHMdfr/Z+DAxHcIBPDuGo3b0g=";
   };
 
   propagatedBuildInputs = [
